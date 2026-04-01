42fc93888132d979f97fc11205593a4e4443dd3e
Borys Ziukov <ziukoff1985@gmail.com>
2026-04-01 12:36:23 +0300

    Finally check

--- server/src/routers/authorsRouters.js
+++ server/src/routers/authorsRouters.js
@@ -1,23 +1,14 @@
 const { Router } = require('express');
 // ----------------------------------
 const authorController = require('../controllers/authorsController');
-// const { validate } = require('../middleware/index');
 const { validateBody } = require('../middleware/index');
 const { AUTHOR_VALIDATION_SCHEMA } = require('../utils/validationSchemas');
 // ----------------------------------
 const router = new Router();
 
-// const {
-//     validateBody: { validateBody },
-// } = middleware;
-
-// const { validateAuthor } = validate;
-
 router
     .route('/')
     .get(authorController.getAllAuthors)
-    // .post(validateAuthor, authorController.createAuthor)
-    // .put(validateAuthor, authorController.updateAuthor);
     .post(validateBody(AUTHOR_VALIDATION_SCHEMA), authorController.createAuthor)
     .put(validateBody(AUTHOR_VALIDATION_SCHEMA), authorController.updateAuthor);
 
